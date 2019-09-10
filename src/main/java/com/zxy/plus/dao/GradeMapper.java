b0b8d0428b0b5dc6c38bafe32b05a68e69aa4459
239694522 <2396945222@qq.com>
2019-09-10 09:05:02 +0800

    333

---
+++ src/main/java/com/zxy/plus/dao/GradeMapper.java
@@ -0,0 +1,10 @@
+package com.zxy.plus.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zxy.plus.entity.Grade;
+@Mapper
+public interface GradeMapper extends BaseMapper<Grade> {
+
+}
