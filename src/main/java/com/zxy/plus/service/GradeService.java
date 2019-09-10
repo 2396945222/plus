34514e761f3146792f049733c49c414d23676d33
239694522 <2396945222@qq.com>
2019-09-10 09:04:40 +0800

    222

---
+++ src/main/java/com/zxy/plus/service/GradeService.java
@@ -0,0 +1,8 @@
+package com.zxy.plus.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zxy.plus.entity.Grade;
+
+public interface GradeService extends IService<Grade>{
+
+}
